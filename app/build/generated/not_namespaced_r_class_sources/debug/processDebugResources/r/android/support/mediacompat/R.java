8c5543e94fedac74f5056dff0acad084669b9874
Aleksandr <alexmuratidi@mail.ru>
2019-04-19 23:50:17 +0300

    startact

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
@@ -28,13 +28,13 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f05007b;
-        public static final int notification_icon_bg_color = 0x7f05007c;
-        public static final int notification_material_background_media_default_color = 0x7f05007d;
-        public static final int primary_text_default_material_dark = 0x7f050085;
-        public static final int ripple_material_light = 0x7f05008a;
-        public static final int secondary_text_default_material_dark = 0x7f05008b;
-        public static final int secondary_text_default_material_light = 0x7f05008c;
+        public static final int notification_action_color_filter = 0x7f05007e;
+        public static final int notification_icon_bg_color = 0x7f05007f;
+        public static final int notification_material_background_media_default_color = 0x7f050080;
+        public static final int primary_text_default_material_dark = 0x7f050088;
+        public static final int ripple_material_light = 0x7f05008d;
+        public static final int secondary_text_default_material_dark = 0x7f05008e;
+        public static final int secondary_text_default_material_light = 0x7f05008f;
     }
     public static final class dimen {
         private dimen() {}
@@ -69,18 +69,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f070084;
-        public static final int notification_bg = 0x7f070085;
-        public static final int notification_bg_low = 0x7f070086;
-        public static final int notification_bg_low_normal = 0x7f070087;
-        public static final int notification_bg_low_pressed = 0x7f070088;
-        public static final int notification_bg_normal = 0x7f070089;
-        public static final int notification_bg_normal_pressed = 0x7f07008a;
-        public static final int notification_icon_background = 0x7f07008b;
-        public static final int notification_template_icon_bg = 0x7f07008c;
-        public static final int notification_template_icon_low_bg = 0x7f07008d;
-        public static final int notification_tile_bg = 0x7f07008e;
-        public static final int notify_panel_notification_icon_bg = 0x7f07008f;
+        public static final int notification_action_background = 0x7f070086;
+        public static final int notification_bg = 0x7f070087;
+        public static final int notification_bg_low = 0x7f070088;
+        public static final int notification_bg_low_normal = 0x7f070089;
+        public static final int notification_bg_low_pressed = 0x7f07008a;
+        public static final int notification_bg_normal = 0x7f07008b;
+        public static final int notification_bg_normal_pressed = 0x7f07008c;
+        public static final int notification_icon_background = 0x7f07008d;
+        public static final int notification_template_icon_bg = 0x7f07008e;
+        public static final int notification_template_icon_low_bg = 0x7f07008f;
+        public static final int notification_tile_bg = 0x7f070090;
+        public static final int notify_panel_notification_icon_bg = 0x7f070091;
     }
     public static final class id {
         private id() {}
@@ -97,27 +97,27 @@ public final class R {
         public static final int chronometer = 0x7f080035;
         public static final int end_padder = 0x7f080050;
         public static final int forever = 0x7f08005b;
-        public static final int icon = 0x7f080063;
-        public static final int icon_group = 0x7f080064;
-        public static final int info = 0x7f08006a;
-        public static final int italic = 0x7f08006c;
-        public static final int line1 = 0x7f080073;
-        public static final int line3 = 0x7f080074;
-        public static final int media_actions = 0x7f080079;
-        public static final int normal = 0x7f080086;
-        public static final int notification_background = 0x7f080087;
-        public static final int notification_main_column = 0x7f080088;
-        public static final int notification_main_column_container = 0x7f080089;
-        public static final int right_icon = 0x7f0800a0;
-        public static final int right_side = 0x7f0800a1;
-        public static final int status_bar_latest_event_content = 0x7f0800cb;
-        public static final int tag_transition_group = 0x7f0800d2;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800d3;
-        public static final int tag_unhandled_key_listeners = 0x7f0800d4;
-        public static final int text = 0x7f0800d5;
-        public static final int text2 = 0x7f0800d6;
-        public static final int time = 0x7f0800de;
-        public static final int title = 0x7f0800df;
+        public static final int icon = 0x7f080064;
+        public static final int icon_group = 0x7f080065;
+        public static final int info = 0x7f08006b;
+        public static final int italic = 0x7f08006d;
+        public static final int line1 = 0x7f080074;
+        public static final int line3 = 0x7f080075;
+        public static final int media_actions = 0x7f08007a;
+        public static final int normal = 0x7f080087;
+        public static final int notification_background = 0x7f080088;
+        public static final int notification_main_column = 0x7f080089;
+        public static final int notification_main_column_container = 0x7f08008a;
+        public static final int right_icon = 0x7f0800a1;
+        public static final int right_side = 0x7f0800a2;
+        public static final int status_bar_latest_event_content = 0x7f0800cc;
+        public static final int tag_transition_group = 0x7f0800d3;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800d4;
+        public static final int tag_unhandled_key_listeners = 0x7f0800d5;
+        public static final int text = 0x7f0800d6;
+        public static final int text2 = 0x7f0800d7;
+        public static final int time = 0x7f0800df;
+        public static final int title = 0x7f0800e0;
     }
     public static final class integer {
         private integer() {}
--- app/src/main/java/com/app/lionnews/activity/StartUpActivity.java
+++ app/src/main/java/com/app/lionnews/activity/StartUpActivity.java
@@ -3,28 +3,73 @@ package com.app.lionnews.activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.app.lionnews.R;
+import com.app.lionnews.util.ScreenUtils;
 
 public class StartUpActivity extends AppCompatActivity {
+
+
+    private Button bonusButton;
+
+    private View rootView;
+
+    double screenWidth;
+    double screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
-        Button button = (Button) findViewById(R.id.startuem);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        rootView = findViewById(R.id.root);
+
+        bonusButton = findViewById(R.id.get_bonus_button);
+
+        screenHeight = ScreenUtils.getHeight(this);
+        screenWidth = ScreenUtils.getWidth(this);
+
+        ConstraintLayout.LayoutParams buttonParams =
+                (ConstraintLayout.LayoutParams) bonusButton.getLayoutParams();
+
+        if (ScreenUtils.isLongScreen(this)) {
+            rootView.setBackground(getDrawable(R.drawable.background_long));
+            bonusButton.setHeight((int) (screenHeight * ScreenUtils.BUTTON_HEIGHT_LONG));
+
+            buttonParams.setMargins(
+                    buttonParams.leftMargin,
+                    buttonParams.topMargin,
+                    buttonParams.rightMargin,
+                    ScreenUtils.pixToDp(ScreenUtils.BUTTON_MARGIN_BOTTOM_LONG
+                            * ScreenUtils.getHeight(this), getApplicationContext()));
+
+            bonusButton.setLayoutParams(buttonParams);
+
+        } else {
+            rootView.setBackground(getDrawable(R.drawable.background_normal));
+            bonusButton.setHeight((int) (screenHeight * ScreenUtils.BUTTON_HEIGHT_NORMAL));
+
+            buttonParams.setMargins(
+                    buttonParams.leftMargin,
+                    buttonParams.topMargin,
+                    buttonParams.rightMargin,
+                    ScreenUtils.pixToDp(ScreenUtils.BUTTON_MARGIN_BOTTOM_NORMAL
+                            * ScreenUtils.getHeight(this), getApplicationContext()));
+
+            bonusButton.setLayoutParams(buttonParams);
+        }
+
+        bonusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Math.random() >= 1)
-                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                else
-                    startActivity(new Intent(getApplicationContext(), BrowserActivity.class));
+                startActivity(new Intent(getApplicationContext(), BrowserActivity.class));
             }
         });
+
     }
 }
